bb80395d82d675a82e54573bc0eeef63feb9868e
Anuj Deshpande <anuj@espressif.com>
2018-08-30 11:31:21 +0530

    Initial commit
     - 4 schemes available, a combination of BLE or Wi-Fi and Sec0 or Sec1
     - Optional proof of possession value in info.plist (only used in the case of Sec1)

---
+++ EspressifProvision/EspressifProvision/ui/ProvisionLandingViewController.swift
@@ -0,0 +1,64 @@
+// Copyright 2018 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//  ProvisionLandingViewController.swift
+//  EspressifProvision
+//
+
+import Foundation
+import UIKit
+
+class ProvisionLandingViewController: UIViewController {
+    var provisionConfig: [String: String] = [:]
+    @IBOutlet var provisionInstructions: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let wifiPrefix = provisionConfig[Provision.CONFIG_WIFI_AP_KEY]
+        if let text = provisionInstructions.text, let wifiPrefix = wifiPrefix {
+            let nonBoldRange = NSMakeRange(0, text.count)
+            provisionInstructions.attributedText = attributedString(from: text + " \(wifiPrefix)",
+                                                                    nonBoldRange: nonBoldRange)
+        }
+    }
+
+    @IBAction func connectClicked(_: Any) {
+        if let url = URL(string: UIApplicationOpenSettingsURLString) {
+            if UIApplication.shared.canOpenURL(url) {
+                let url = UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender _: Any?) {
+        if let vc = segue.destination as? ProvisionViewController {
+            vc.provisionConfig = provisionConfig
+        }
+    }
+
+    func attributedString(from string: String, nonBoldRange: NSRange?) -> NSAttributedString {
+        let attrs = [
+            kCTFontAttributeName: UIFont.boldSystemFont(ofSize: 20),
+            kCTForegroundColorAttributeName: UIColor.black,
+        ]
+        let nonBoldAttribute = [
+            kCTFontAttributeName: UIFont.systemFont(ofSize: 20),
+        ]
+        let attrStr = NSMutableAttributedString(string: string, attributes: attrs as [NSAttributedStringKey: Any])
+        if let range = nonBoldRange {
+            attrStr.setAttributes(nonBoldAttribute as [NSAttributedStringKey: Any], range: range)
+        }
+        return attrStr
+    }
+}
