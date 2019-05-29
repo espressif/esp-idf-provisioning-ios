e8fb349fa42b9f7787eff2c4f0aab5905c4f0771
Vikas Chandra <vikas.chandra@espressif.com>
2019-05-29 17:14:30 +0530

    ALEXA-241 - Local discovery and listing of devices using ssdp.

---
+++ EspressifProvision/EspressifProvision/Utilities/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  EspressifProvision
+//
+//  Created by Vikas Chandra on 28/05/19.
+//  Copyright © 2019 Espressif. All rights reserved.
+//
+
+import Foundation
+import MBProgressHUD
+import UIKit
+
+struct Constants {
+    static let friendlynameKey = "friendlyname"
+    static let UUIDKey = "uuid"
+
+    // Reuse identifier
+    static let deviceListCellReuseIdentifier = "deviceListCell"
+
+    static func showLoader(message: String, view: UIView) {
+        DispatchQueue.main.async {
+            let loader = MBProgressHUD.showAdded(to: view, animated: true)
+            loader.mode = MBProgressHUDMode.indeterminate
+            loader.label.text = message
+        }
+    }
+}
---
+++ EspressifProvision/EspressifProvision/ui/ManageDevice/DeviceListTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DeviceListTableViewCell.swift
+//  EspressifProvision
+//
+//  Created by Vikas Chandra on 29/05/19.
+//  Copyright © 2019 Espressif. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DeviceListTableViewCell: UITableViewCell {
+    @IBOutlet var deviceNameLabel: UILabel!
+}
